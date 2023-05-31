21f808998dbad62fbc512cbf3e3b82f2791527d6
Abdullah Najeeb <abdullahnajeeb0306@gmail.com>
2023-05-31 04:27:39 +0000

    fact table creation

--- models/example/raw_zone/fact_table.sql
+++ models/example/raw_zone/fact_table.sql
@@ -1,91 +1,64 @@
 {{ config(materialized="table") }}
 
-with fact_table as 
-(
+
+with fact_table as (
     select * from healthcare.star_schema.fact_table
-)
-select * from fact_table
-
-
-
-
-
-{#
-
-{{ config(materialized="table") }}
-
-with fct_id as 
-(
-    select * from {{ ref('fct_id') }}
 ),
-
-dim_date as
-(
+dim_date as (
     select * from {{ ref('dim_date') }}
 ),
-
-dim_diagnosis as
-(
+dim_diagnosis as (
     select * from {{ ref('dim_diagnosis') }}
 ),
-
-dim_location as 
-(
+dim_location as (
     select * from {{ ref('dim_location') }}
 ),
-
-dim_patient as
-(
+dim_patient as (
     select * from {{ ref('dim_patient') }}
 ),
-
-dim_payer as 
-(
+dim_payer as (
     select * from {{ ref('dim_payer') }}
 ),
-
-dim_physician as
-(
+dim_physician as (
     select * from {{ ref('dim_physician') }}
 ),
-
-dim_transaction as 
-(
+dim_transaction as (
     select * from {{ ref('dim_transaction') }}
 ),
-
-dim_cptcode as 
-(
+dim_cptcode as (
     select * from {{ ref('dim_cptcode') }}
 ),
 
-fact_table as 
-(
+fact_table_comb as (
     select 
-    fct_id.fact_id,
-    dim_cptcode.CPTCode_id,
-    dim_date.DatePost_id,
-    dim_location.location_id,
-    dim_patient.patient_id,
-    dim_payer.payer_id,
-    dim_physician.physician_id,
-    dim_transaction.transaction_id
+        fact_table.fact_id,
+        dim_cptcode.CPTCode_id,
+        dim_date.DatePost_id,
+        dim_location.location_id,
+        dim_location.locationname,
+        dim_patient.patient_id,
+        dim_payer.payer_id,
+        dim_physician.physician_id,
+        dim_transaction.transaction_id
+   FROM
+    healthcare.star_schema.fact_table
 
-
-    from 
-    fct_id
-    FULL OUTER  join dim_cptcode on fct_id.fact_id = fct_id.fact_id 
-    FULL OUTER join dim_date on fct_id.fact_id= fct_id.fact_id
-    full OUTER join dim_transaction on fct_id.fact_id= fct_id.fact_id
-    full OUTER join dim_location on fct_id.fact_id= fct_id.fact_id
-    full OUTER join dim_patient on fct_id.fact_id= fct_id.fact_id
-    full OUTER join dim_payer on fct_id.fact_id= fct_id.fact_id
-    full OUTER join dim_physician on fct_id.fact_id= fct_id.fact_id 
-
+LEFT JOIN
+    DIM_PATIENT ON FACT_TABLE.DIM_PATIENT_ID = DIM_PATIENT.PATIENT_ID
+LEFT JOIN
+    DIM_PHYSICIAN ON FACT_TABLE.DIM_PHYSICIAN_ID = DIM_PHYSICIAN.PHYSICIAN_ID
+LEFT JOIN
+    DIM_DATE ON FACT_TABLE.DIM_DATE_ID = DIM_DATE.DATEPOST_ID
+LEFT JOIN
+    DIM_CPTCODE ON FACT_TABLE.DIM_CPTCODE_ID = DIM_CPTCODE.CPTCODE_ID
+LEFT JOIN
+    DIM_PAYER ON FACT_TABLE.DIM_PAYER_ID = DIM_PAYER.PAYER_ID
+LEFT JOIN
+    DIM_TRANSACTION ON FACT_TABLE.DIM_TRANSACTION_ID = DIM_TRANSACTION.TRANSACTION_ID
+LEFT JOIN  
+    DIM_LOCATION ON FACT_TABLE.DIM_LOCATION_ID = DIM_LOCATION.LOCATION_ID
+LEFT JOIN
+    DIM_DIAGNOSIS ON FACT_TABLE.DIM_DIAGNOSIS_ID = DIM_DIAGNOSIS.DIAGNOSISCODE_ID
 )
 
-select * from fact_table
-limit 10000
-
-#}
-
+select * from fact_table_comb
